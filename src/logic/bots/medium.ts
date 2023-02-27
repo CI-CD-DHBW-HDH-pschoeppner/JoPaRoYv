35b8d70274d55effc2092f13759d0edbfc646ae8
iOsShadow <77612730+iOsShadow@users.noreply.github.com>
2023-02-27 11:37:08 +0100

    implement pettyMove

--- src/logic/bots/medium.ts
+++ src/logic/bots/medium.ts
@@ -1,4 +1,5 @@
-import type { Field } from "../game";
+import { Field } from "../game";
+import { randomMove, winningMove } from "./bot";
 
 // the medium bot:
 // - chooses the winning move, if it can win
@@ -13,5 +14,17 @@ export function mediumMove(board: Field[], own: Field): number {
 // - blocks the player from winning, if it can
 // - chooses a random move otherwise
 export function pettyMove(board: Field[], own: Field): number {
-  return -1 // TODO: implement
+    const isFull = (currentValue: Field) => currentValue != Field.EMPTY
+    if(board.every(isFull)){
+        return -1
+    }
+    var enemy = (own==Field.PLAYER1)?Field.PLAYER1:Field.PLAYER2
+    if(winningMove(board, enemy) != -1){
+        return winningMove(board, enemy)
+    } else{
+        do{
+            var move = randomMove(board.length)
+        }while(board[move] != Field.EMPTY)
+        return move
+    }
 }
