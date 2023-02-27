0df1aa2eebcf41aefb12ab7319b2bfbb49518851
jp <19jonasproell99@gmail.com>
2023-02-27 11:46:08 +0100

    refactoring

--- src/logic/bots/bot.ts
+++ src/logic/bots/bot.ts
@@ -33,27 +33,33 @@ export function winningMove(board: Field[], player: Field): number {
 
   player = invertPlayer(player);
 
-  console.log(player, board);
-
   // horicontally
   for (let i = 0; i < 3; i++) {
+    // get rows horicontally
     let offset = i * 3;
     const row = board.slice(offset, offset + 3);
 
+    // skip if no empty fields available
     if (row.filter((r) => r === Field.EMPTY).length !== 1) continue;
 
-    const count = row.filter((r) => r === player).length;
-    if (count == 2) return offset + row.indexOf(Field.EMPTY);
+    // skip if player does not already have two fields
+    if (row.filter((r) => r === player).length != 2) continue;
+
+    return offset + row.indexOf(Field.EMPTY);
   }
 
   // vertically
   for (let i = 0; i < 3; i++) {
+    // get rows vertically
     const row = [board[i], board[i + 3], board[i + 6]];
 
+    // skip if no empty fields available
     if (row.filter((r) => r === Field.EMPTY).length !== 1) continue;
 
-    const count = row.filter((r) => r === player).length;
-    if (count == 2) return row.indexOf(Field.EMPTY) * 3 + i;
+    // skip if player does not already have two fields
+    if (row.filter((r) => r === player).length != 2) continue;
+
+    return row.indexOf(Field.EMPTY) * 3 + i;
   }
 
   return -1;
