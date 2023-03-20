9db386f0733ec3b672bd9d03f4d5e9eb326ea492
JP <proellj.tin20@student.dhbw-heidenheim.de>
2023-03-20 14:28:14 +0100

    changed custom alphabet of nanoid

--- src/logic/online/online.ts
+++ src/logic/online/online.ts
@@ -1,5 +1,5 @@
 import { customAlphabet } from "nanoid";
-const nanoid = customAlphabet("1234567890abcdefghijklmnopqrstuvwxyz", 5);
+const nanoid = customAlphabet("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 5);
 
 export function generateID(): string {
   return nanoid();
